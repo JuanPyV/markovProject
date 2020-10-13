a8c136a16a99bc80c828b3f68116f198f647f973
actuallyfabian <actuallyfabian@gmail.com>
2020-10-12 20:29:41 -0500

    almost done

--- tutorial.py
+++ tutorial.py
@@ -8,7 +8,6 @@ import itertools
 import os
 import glob
 import os.path as path
-"""holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"""
 
 
 
@@ -33,62 +32,97 @@ class HMMTrainer(object):
 
 
 
-hmm_models = []
-input_folder = 'genres/'
-# Parse the input directory
-for dirname in os.listdir(input_folder):
-    # Get the name of the subfolder
-   subfolder = os.path.join(input_folder, dirname)
-   if not os.path.isdir(subfolder):
-          continue
-      # Extract the label
-   label = subfolder[subfolder.rfind('/') + 1:]
-    # Initialize variables
-   X = np.array([])
-   y_words = []
-   # Iterate through the audio files (leaving 1 file for testing in each class)
-   for filename in [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]:
-      # Read the input file
-      filepath = os.path.join(subfolder, filename)
-      sampling_freq, audio = wavfile.read(filepath)
-      # Extract MFCC features
-      mfcc_features = mfcc(audio, sampling_freq)
-      # Append to the variable X
-      if len(X) == 0:
-         X = mfcc_features
-      else:
-         X = np.append(X, mfcc_features, axis=0)
+# hmm_models = []
+# input_folder = 'genres/'
+# # Parse the input directory
+# for dirname in os.listdir(input_folder):
+#     # Get the name of the subfolder
+#    subfolder = os.path.join(input_folder, dirname)
+#    if not os.path.isdir(subfolder):
+#           continue
+#       # Extract the label
+#    label = subfolder[subfolder.rfind('/') + 1:]
+#     # Initialize variables
+#    X = np.array([])
+#    y_words = []
+#    # Iterate through the audio files (leaving 1 file for testing in each class)
+#    for filename in [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]:
+#       # Read the input file
+#       filepath = os.path.join(subfolder, filename)
+#       sampling_freq, audio = wavfile.read(filepath)
+#       # Extract MFCC features
+#       mfcc_features = mfcc(audio, sampling_freq)
+#       # Append to the variable X
+#       if len(X) == 0:
+#          X = mfcc_features
+#       else:
+#          X = np.append(X, mfcc_features, axis=0)
 
-            # Append the label
-      y_words.append(label)    
-   print('X.shape =', X.shape)
-    # Train and save HMM model
-   hmm_trainer = HMMTrainer(n_components=10)
-   hmm_trainer.train(X)
-   hmm_models.append((hmm_trainer, label))
-   hmm_trainer = None
+#             # Append the label
+#       y_words.append(label)    
+#    print('X.shape =', X.shape)
+#     # Train and save HMM model
+#    hmm_trainer = HMMTrainer(n_components=10)
+#    hmm_trainer.train(X)
+#    hmm_models.append((hmm_trainer, label))
+#    hmm_trainer = None
 
-input_folder = 'test/'
-real_labels = []
-pred_labels = []
-for dirname in os.listdir(input_folder):
-   subfolder = os.path.join(input_folder, dirname)
-   if not os.path.isdir(subfolder):
-      continue
-   # Extract the label
-   label_real = subfolder[subfolder.rfind('/') + 1:]
+# input_folder = 'test/'
+# real_labels = []
+# pred_labels = []
+# for dirname in os.listdir(input_folder):
+#    subfolder = os.path.join(input_folder, dirname)
+#    if not os.path.isdir(subfolder):
+#       continue
+#    # Extract the label
+#    label_real = subfolder[subfolder.rfind('/') + 1:]
 
-   for filename in [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]:
-      real_labels.append(label_real)
-      filepath = os.path.join(subfolder, filename)
-      sampling_freq, audio = wavfile.read(filepath)
-      mfcc_features = mfcc(audio, sampling_freq)
-      max_score = -9999999999999999999
-      output_label = None
-      for item in hmm_models:
-         hmm_model, label = item
-         score = hmm_model.get_score(mfcc_features)
-         if score > max_score:
-            max_score = score
-            output_label = label
-pred_labels.append(output_label)
+#    for filename in [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]:
+#       real_labels.append(label_real)
+#       filepath = os.path.join(subfolder, filename)
+#       sampling_freq, audio = wavfile.read(filepath)
+#       mfcc_features = mfcc(audio, sampling_freq)
+#       max_score = -9999999999999999999
+#       output_label = None
+#       for item in hmm_models:
+#          hmm_model, label = item
+#          score = hmm_model.get_score(mfcc_features)
+#          if score > max_score:
+#             max_score = score
+#             output_label = label
+# pred_labels.append(output_label)
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+   if normalize:
+      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+      print("Normalized confusion matrix")
+   else:
+      print('Confusion matrix, without normalization')
+
+   print(cm)
+
+   plt.imshow(cm, interpolation='nearest', cmap=cmap)
+   plt.title(title)
+   plt.colorbar()
+   tick_marks = np.arange(len(classes))
+   plt.xticks(tick_marks, classes, rotation=45)
+   plt.yticks(tick_marks, classes)
+
+   fmt = '.2f' if normalize else 'd'
+   thresh = cm.max() / 2.
+   for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+   plt.tight_layout()
+   plt.ylabel('True label')
+   plt.xlabel('Predicted label')
+
+
+cm = confusion_matrix(real, pred)
+np.set_printoptions(precision=2)
+classes = ["blues","classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
+plt.figure()
+plot_confusion_matrix(cm, classes=classes, normalize=True, title='Normalized confusion matrix')
+
+plt.show()
